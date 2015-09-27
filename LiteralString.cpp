641b11d983be920b35db95d97af4157ea26ceed9
rododo <lzn960520@126.com>
2015-09-28 00:15:11 +0800

    add support for escape char

--- LiteralString.cpp
+++ LiteralString.cpp
@@ -1,7 +1,113 @@
 #include "yyvaltypes.h"
+#include "exception.h"
 
-LiteralString::LiteralString(const char *text) :
-	text(text) {
+static inline int char2hex(const char a) {
+	switch (a) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+		return a - '0';
+	case 'a':
+	case 'b':
+	case 'c':
+	case 'd':
+	case 'e':
+	case 'f':
+		return a - 'a' + 10;
+	case 'A':
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'E':
+	case 'F':
+		return a - 'A' + 10;
+	default:
+		return -1;
+	}
+}
+LiteralString::LiteralString(const char *ori_text) :
+	text(strlen(ori_text), '\0') {
+	ori_text++;
+	char *p;
+	for (p = (char *) text.data(); *ori_text && (*ori_text) != '\"'; ori_text++, p++) {
+		if (*ori_text != '\\')
+			*p = *ori_text;
+		else {
+			switch (*(ori_text + 1)) {
+			case 'a':
+				*p = '\a';
+				ori_text++;
+				break;
+			case 'b':
+				*p = '\b';
+				ori_text++;
+				break;
+			case 'f':
+				*p = '\f';
+				ori_text++;
+				break;
+			case 'n':
+				*p = '\n';
+				ori_text++;
+				break;
+			case 'r':
+				*p = '\r';
+				ori_text++;
+				break;
+			case 't':
+				*p = '\t';
+				ori_text++;
+				break;
+			case 'v':
+				*p = '\v';
+				ori_text++;
+				break;
+			case '\\':
+				*p = '\\';
+				ori_text++;
+				break;
+			case '\'':
+				*p = '\'';
+				ori_text++;
+				break;
+			case '\"':
+				*p = '\"';
+				ori_text++;
+				break;
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+				ori_text++;
+				*p = 0;
+				while ((*ori_text <= '9') && (*ori_text >= '0'))
+					*p = *p * 8 + *ori_text++ - '0';
+				ori_text--;
+				break;
+			case 'x':
+				if (char2hex(*(ori_text + 2)) == -1 || char2hex(*(ori_text + 3)) == -1)
+					throw InvalidString("Invalid \\x escape");
+				*p = (char2hex(*(ori_text + 2)) << 4) + char2hex(*(ori_text + 3));
+				ori_text += 3;
+				break;
+			default:
+				*p = *(++ori_text);
+				break;
+			}
+		}
+	}
+	text = text.substr(0, p - text.data());
 }
 
 Json::Value LiteralString::json() {
